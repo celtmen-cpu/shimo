6958e455a02d088a3e940c1caaf7b8a92c1fcb54
celtmen <achintoutain.clement@icloud.com>
2026-07-06 19:20:43 +0200

    Remove alert on button click for download

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 document.getElementById("btn").addEventListener("click", function () {
   window.location.href = "https://github.com/celtmen-cpu/shimo/releases/download/App-v1/app-shimo.apk";
-  alert("Application Téléchargée");
 });
